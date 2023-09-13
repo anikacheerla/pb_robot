6d135bd5e8d8c4a8582c8e858353dd66e9ec9c7b
gregoryxie <ggxxie@gmail.com>
2023-09-13 15:57:38 -0400

    Not working r-prm

--- src/pb_robot/planners/prm.py
+++ src/pb_robot/planners/prm.py
@@ -10,8 +10,6 @@ from .util import INF, merge_dicts, flatten, bisect
 
 import pb_robot
 
-# TODO - Visibility-PRM, PRM*
-
 class Vertex(object):
     def __init__(self, q):
         self.q = q
@@ -62,6 +60,7 @@ class Edge(object):
         return 'Edge(' + str(self.v1.q) + ' - ' + str(self.v2.q) + ')'
     __repr__ = __str__
 
+
 class TimeVertex(Vertex):
     def __init__(self, q):
         super(self.__class__, self).__init__(q)
@@ -77,10 +76,11 @@ class TimeVertex(Vertex):
                 return False, tup[1]
         return True, 0
 
-class TimeEdge(Edge):
+
+class ConfEdge(Edge):
     def __init__(self, v1, v2, path):
         super(self.__class__, self).__init__(v1, v2, path)
-        self.times = [] # times the edge is available
+        self.configurations = set() # configurations the edge is unavailable
     
     def path(self, start, t1=0, t2=0):
         if self._path is None:
@@ -91,13 +91,13 @@ class TimeEdge(Edge):
             return [(q, t) for q, t in zip(self._path[::-1], np.linspace(t1, t2, len(self._path)))] + [(self.v1.q, t2)]
         assert False
 
-    def is_edge_active(self, t, duration=0):
-        # TODO: can be much much smarter about this, change to binary search, MRU ordering
-        # make sure that t -> duration isn't overlapping with 
-        for tup in self.times:
-            if t <= tup[1] and t >= tup[0]: # in a collision time
-                return False, tup[1] # when edge will next be free
-        return True, 0
+    def is_edge_active(self, dynamic_obstacles_conf):
+        # dynamic_obstacles_conf = [(id, location), (id, location)]
+        for tup in dynamic_obstacles_conf:
+            # TODO: can use rounding to see if obstacles are close enough 
+            if tup in self.configurations:
+                return False
+        return True
 
 ##################################################
 
@@ -241,99 +241,32 @@ class TPRM(PRM):
             distance_fn, extend_fn, collision_fn, samples=samples)
         
         self.json_file = json_file
-        # self.grow(samples)
-        if json_file:
-            self.load_graph(json_file)
-        else:
-            self.grow(samples)
-
-    
-    def load_graph(self, json_file):
-        import json
-        with open(json_file, 'r') as openfile:
-            # Reading from json file
-            json_object = json.load(openfile)
-            
-
-            for i, val in json_object.items():
-                v1 = TimeVertex(np.array(val[0]))
-                v2 = TimeVertex(np.array(val[1]))
-                path = val[2]
-                times = val[3]
-                edge = TimeEdge(v1, v2, path)
-                edge.times = times
-                self.edges.append(edge) 
-
-    def save_graph(self, json_file):
-        import json
-        graph = dict()
-
-        # Save each edge: q1, q2: path, times
-        # initialize vertices by looping through edges, intialize times
-
-        for i, e in enumerate(self.edges): 
-            graph[i] = (e.v1.q.tolist(), e.v2.q.tolist(), e._path, e.times)
-        with open(json_file, 'w') as outfile:
-            json.dump(graph, outfile)
+        self.grow(samples)
                 
     def duration(self, v1, v2):
-        # Calculate time taken to get from q1 to q2
-        # Everything takes 1 sec per unti distance for now
+        # Everything takes 1 sec per unit distance for now
         if v1 == v2:
             return 1
         return self.distance_fn(v1.q, v2.q) # * robot movement speed
-    
-    def calculate_time_availabilities(self, vertices, dynamic_obstacles, timestep=0.5, max_time=1.5):
-        if not dynamic_obstacles:
-            return
+        
+    def calculate_edge_availability(self, edges, dynamic_obstacles):
+        # assume function that calls calculate_edge_availability resets dynamic_obstacles
+        # each edge will have a set of dynamic obstacle confs in which it is unavailable
+        # how can I use paths instead?
 
-        collision_start = [-1]*len(vertices)
-        for t in np.arange(0, max_time, timestep):
-            for obs in dynamic_obstacles: 
-                obs.set_point(obs.get_position(t))
-
-            for i, v in enumerate(vertices):
-                # in collision
-                if collision_start[i] == -1 and not self.manip.IsCollisionFree(v.q, obstacles=dynamic_obstacles):
-                    collision_start[i] = t
-                # out of collision
-                if collision_start[i] != -1 and self.manip.IsCollisionFree(v.q, obstacles=dynamic_obstacles):
-                    v.times.append((collision_start[i], t))
-                    collision_start[i] = -1
-            
-        for obs in dynamic_obstacles:
-            obs.set_point(obs.get_position(0))
-
-
-        for i, v in enumerate(vertices):
-            if collision_start[i] != -1:
-                v.times.append((collision_start[i], max_time))
-    
-    def calculate_edge_time_availabilities(self, edges, dynamic_obstacles, timestep=0.5, max_time=1.5):
+        # TODO: could save next open configuration
         if not dynamic_obstacles:
             return 
-        
-        collision_start = [-1]*len(edges)
-        for t in np.arange(0, max_time, timestep):
-            for obs in dynamic_obstacles:
-                obs.set_point(obs.get_position(t))
-            for i, e in enumerate(edges):
-                if collision_start[i] == -1:
-                    # in collision
-                    if any (not self.manip.IsCollisionFree(q, obstacles=dynamic_obstacles) for q in bisect(e._path)):
-                        collision_start[i] = t
-                elif all (self.manip.IsCollisionFree(q, obstacles=dynamic_obstacles) for q in bisect(e._path)):
-                    # now not collision free
-                    e.times.append((collision_start[i], t))
-                    collision_start[i] = -1
-
-        for obs in dynamic_obstacles:
-            obs.set_point(obs.get_position(0))
-
         for i, e in enumerate(edges):
-            if collision_start[i] != -1:
-                e.times.append((collision_start[i], max_time))
-
+            for obs in dynamic_obstacles:
+                if any (not self.manip.IsCollisionFree(q, obstacles=[obs]) for q in bisect(e._path)):
+                    e.configurations.add((obs.get_name(), obs.get_point())) # id and location at which dynamic obstacle collides w/ edge
+    
+    def get_confs_at_time(self, t):
+        confs = []
+        for obs in self.dynamic_obstacles:
+            confs.append(obs.get_name(), obs.get_position(t))
+        return confs
 
     def __call__(self, q1, q2, start_time=0):
         # TODO: start_time is global time at which previous path ended
@@ -359,7 +292,7 @@ class TPRM(PRM):
             # print(arrival_times[v])
             if nodes[v].parent is None:
                 assert arrival_times[v] == start_time
-                return [(v.q, arrival_times[v])]
+                return np.array([v.q, arrival_times[v]])
             parent = nodes[v].parent
             return retrace_with_times(parent) + v.edges[parent].path(parent, arrival_times[parent], arrival_times[v])
         
@@ -373,26 +306,19 @@ class TPRM(PRM):
                 return retrace_with_times(cv), t # return end_time as well
             for nv, edge in cv.edges.items():
                 duration = self.duration(cv, nv)
-                is_active, next_free = edge.is_edge_active(t+duration)
+                dynamic_obtacle_confs = self.get_confs_at_time(t+duration)
+                is_active = edge.is_edge_active(dynamic_obtacle_confs)
                 if is_active:
-                    # print("Time:" ,t+duration)
-
                     cost = nodes[cv].cost + self.distance_fn(cv.q, nv.q)
                     if (nv not in nodes) or (cost < nodes[nv].cost):
                         nodes[nv] = SearchNode(cost, cv)
                         arrival_times[nv] = t + duration
                         if cv != nv:
                             heappush(queue, (cost + heuristic(nv), nv))
+                
+                # TODO: does it help to wait until that edge is active?
+                # else: try staying at same configuration for a timestep
 
-                else: # does it help to wait until that edge is active?
-                    wait_time = next_free - (t + duration) # find next free interval of node
-                    assert nv.is_active(next_free)[0]
-                    cost = nodes[cv].cost + self.distance_fn(cv.q, nv.q) + 0.01*wait_time # function of wait time gives wait penalty
-                    if (nv not in nodes) or (cost < nodes[nv].cost):
-                        nodes[nv] = SearchNode(cost, cv)
-                        arrival_times[nv] = next_free
-                        if cv != nv:
-                            heappush(queue, (cost + heuristic(nv), nv))
         print("Plan motion FAILED")
         # import IPython
         # IPython.embed()
@@ -400,7 +326,7 @@ class TPRM(PRM):
     
     def connect(self, v1, v2, path=None):
         if v1 not in v2.edges:  # TODO - what about parallel edges?
-            edge = TimeEdge(v1, v2, path)
+            edge = ConfEdge(v1, v2, path)
             self.edges.append(edge)
             return edge
         return None
@@ -410,7 +336,6 @@ class TPRM(PRM):
         new_vertices = self.add(samples)
 
         new_edges = []
-
         for i, v1 in enumerate(new_vertices):
             for v2 in new_vertices[i+1:] + list(old_vertices):
                 if (v1 == v2):
@@ -422,10 +347,19 @@ class TPRM(PRM):
                         edge = self.connect(v1, v2, path)
                         if edge:
                             new_edges.append(edge)
+        print("Done connecting edges")
         
-        self.calculate_edge_time_availabilities(new_edges, self.dynamic_obstacles)
-        if not self.json_file:
-            self.save_graph("graph_hard.json")
+        # TODO: edit so these values aren't hardcoded
+        for t in np.arange(0, 1.5, 0.1):
+            for obs in self.dynamic_obstacles:
+                obs.set_point(obs.get_position(t))
+            self.calculate_edge_availability(new_edges, self.dynamic_obstacles)
+
+        print("Done calculating edge availabilities")
+
+        for obs in self.dynamic_obstacles:
+            obs.set_point(obs.get_position(0))
+
         return new_vertices
 
     def add(self, samples):
@@ -433,12 +367,7 @@ class TPRM(PRM):
 
         for q in samples:
             if q not in self:
-                self[q] = TimeVertex(q) # should initialize with time avails
+                self[q] = TimeVertex(q)
                 new_vertices.append(self[q])
-        
-        # self.calculate_time_availabilities(new_vertices, self.dynamic_obstacles)
-        # for v in new_vertices:
-        #     if len(v.times) > 0:
-        #         print(v.times)
 
         return new_vertices
